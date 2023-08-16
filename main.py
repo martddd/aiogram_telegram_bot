c12ecfe567ea67c472eef9636cb79f1293e46f17
martddd <martddd@gmail.com>
2023-08-16 13:16:50 +0000

    function change main
    add date

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def main():
-  date_now = datetime.now()
-  print(f"Привет сейчас {date_now}")
+  date_now = datetime.now() + timedelta(hours=3)
+
+  day = date_now.day
+  month = date_now.month
+  year = date_now.year
+
+  hour = date_now.hour
+  minute = date_now.minute
+
+  print(f"Привет сейчас {hour}:{minute}")
 
 
 if __name__ == "__main__":
